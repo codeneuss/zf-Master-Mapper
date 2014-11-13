de7ef86940e05484183d15f75af1e24c3a722bce
Volkmar Eigler <volkmar.eigler@posteo.de>
2014-11-13 14:54:51 +0100

    Update Mapper.php
    Add Link zu Dokumentation

--- library/Master/Mapper.php
+++ library/Master/Mapper.php
@@ -4,6 +4,7 @@
  * @author Volkmar Eigler <volkmar.eigler@posteo.de>
  * @copyright (c) 2012 Volkmar Eigler
  * @license MIT
+ * @link https://github.com/veigler/zf-Master-Mapper
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
